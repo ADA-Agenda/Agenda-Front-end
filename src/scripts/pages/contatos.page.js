d0fbf501512abc966f3d5fdb3363701d6932a4b6
Dâmares Bortolucci de Sousa <damares_sp78@yahoo.com.br>
2022-11-24 06:49:09 -0300

    add lista de contatos na pag Contatos

--- src/scripts/pages/contatos.page.js
+++ src/scripts/pages/contatos.page.js
@@ -5,34 +5,106 @@ const root = document.getElementById('root')
 
 const contacts = document.createElement('div')
 contacts.setAttribute('id', 'p-contacts')
+/* contacts.setAttribute('class', 'contacts__container') */
+
+const contactsContainer = document.createElement('div')
+contactsContainer.setAttribute('class', 'contacts__container')
+
+let data=[]
+
 
 
 const getContacts = async () => {
     const response = await ContactGet()
-    console.log(response);
 
-    if(response.status === 200) {
-        alert("FOI");
-    }     
+    if(response.status === 200){
+        populateList(response.data)
+        createSearchArea()
+        data = response.data
+    } 
 }
 
 
-const events = () => {
-    contacts.addEventListener('load', getContacts)
+const populateList = contactsArray => {
+
+    const arrayList = contactsArray.map((contact)=>{
+        return `
+        <div class="contacts__card" id="${contact.id}">
+            <div>
+                <div class="contacts__img">
+                    <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460__340.png" alt="Foto do contato">
+                </div>
+                <div class="contacts__data">
+                    <p>${contact.nome}</p>
+                    <span>${contact.email}</span>
+                    <div>
+                        ${contact.telefones.map(tel => `<span>${tel.numero}</span>`).join(" ")}
+                    </div>   
+                </div>
+            </div>
+            <div class="contacts__options">
+                <button>
+                    <i class="fa fa-trash"></i>
+                </button>
+            </div>
+        </div>
+        `
+    })
+    const listHtml = arrayList.join(" ");
+    contactsContainer.insertAdjacentHTML("beforeend", listHtml);
+
+    const buttons = document.querySelectorAll('.contacts__options button')
+    buttons.forEach(b => b.addEventListener('click', deleteContact))
 }
+
+
+
+const createSearchArea = () => {
+
+    const menu = `
+            <input type="text" id="search" placeholder="Buscar contato">
+            <button class="btn-busca">
+                Buscar
+            </button>
+            `
+    contacts.insertAdjacentHTML('beforeend', menu)  
+    const btnSearch = document.querySelector('.btn-busca')
+    btnSearch.addEventListener('click', searchContact)
+}
+
+
+
+const deleteContact = () =>{
+    console.log('DELETOU');
+}
+
+
+const searchContact = () =>{
+    const inputSearch = document.querySelector('#search')
+    const value = inputSearch.value
+    
+    if(value){
+        const searchFilter = data.filter(contact => contact.nome.toLowerCase().includes(value.toLowerCase()))
+
+        if(searchFilter.length !== 0){
+            contactsContainer.innerHTML = "";
+            populateList(searchFilter)
+        } 
+        else
+            alert("Nenhum contato encontrado")
+    } 
+
+    inputSearch.value=""
+}
+
 
 
 export const Contatos = () => {
     const header = Header()
     root.append(header)
 
+    contacts.appendChild(contactsContainer)
     
-    contacts.innerHTML = (`
-        <a id="btn__criar" href="#criar-contato">
-            Criar contato
-        </a>
-    `)
-    
-    events()
+    getContacts()
     return contacts
 }
--- src/scripts/services/contact.service.js
+++ src/scripts/services/contact.service.js
@@ -1,10 +1,12 @@
 
 const baseUrl = 'http://localhost:5000/v1/'
 
-const headers = new Headers()
-headers.append('Content-Type', 'application/json')
+
 
 export const ContactGet = async () => {
+    const headers = new Headers()
+    headers.append('Content-Type', 'application/json')  
+
     const token = sessionStorage.getItem("@token")
     headers.append('Authorization', token)
     const response = await fetch(baseUrl + 'contact', { headers, method: "GET" })
@@ -12,7 +14,38 @@ export const ContactGet = async () => {
 }
 
 
-export const ContactPost = async () => {
-  
+export const ContactPost = async (formData) => {
+    const headers = new Headers()
+    headers.append('Content-Type', 'application/json')
+    const token = sessionStorage.getItem("@token");
+    headers.append('Authorization', token);
+    const contato = ArrangeObject(formData);
+    const body = JSON.stringify(contato);
+    const response = await fetch(baseUrl + 'contact', { body, headers, method: "POST" })
+    return await response.json()
+
+}
+
+function ArrangeObject(formData) {
+
+    const entries = Object.fromEntries(formData);
+
+    const contato = {
+        nome: entries.nome,
+        apelido:entries.apelido,
+        telefones:[{tipo:"celular", numero: entries.celular}, {tipo:"casa", numero: entries.casa} ,{tipo:"trabalho", numero: entries.trabalho} ],
+        email: entries.email,
+        endereco : {
+            cep : entries.cep,
+            logradouro : entries.logradouro,
+            estado : entries.estado,
+            pais : entries.pais,
+            cidade : entries.pais
+        },
+        notas: entries.notas,
+        foto: ""
+    }
+    return contato;
+
 }
 
